da9f713783bc65d1bf95a612adc0a5c42121450d
DESKTOP-EFIJVV4\hsidh <hsidhu@my.gcu.edu>
2020-09-21 18:58:02 -0700

    calendar added. pending week of the month.

---
+++ Pages/Schedules/CreateShifts.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shift_Picker.Pages.Schedules
+{
+    public class CreateShiftsModel : PageModel
+    {
+        public string CurrentDate { get; set; }
+        public CreateShiftsModel()
+        {
+
+        }
+        public void OnGet()
+        {
+
+        }
+
+
+
+        public void GetWeek()
+        {
+            
+            DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+            Calendar cal = dfi.Calendar;
+
+            int currentWeek = cal.GetWeekOfYear(DateTime.Now, dfi.CalendarWeekRule, dfi.FirstDayOfWeek);
+
+        }
+    }
+}

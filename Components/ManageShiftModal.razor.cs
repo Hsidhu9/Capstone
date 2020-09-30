3082cd66e8d17eecee6682b1113f87f02949db50
DESKTOP-EFIJVV4\hsidh <hsidhu@my.gcu.edu>
2020-09-30 00:28:39 -0700

    Drop shift Modal

---
+++ Components/ManageShiftModal.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shift_Picker.Components
+{
+    public class ManageShiftModalMV: OwningComponentBase
+    {
+        protected async override Task OnInitializedAsync()
+    {
+
+    }
+
+    }
+
+}

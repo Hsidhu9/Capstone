7d40e512f04540e5b33439f01647b6356fd0c4ed
Parneet Kaur <parneet.kaur@scheduleviewer.com>
2020-10-10 16:15:48 -0700

    Profile Page

---
+++ Components/UserProfile.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using ShiftPicker.Data.Models;
+using ShiftPicker.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shift_Picker.Components
+{
+    public partial class UserProfileMV : OwningComponentBase
+    {
+        private IUserService UserService => ScopedServices.GetService<IUserService>();
+
+        protected List<UserModel> AllUsers { get; set; } = new List<UserModel>();
+
+        protected async override Task OnInitializedAsync()
+        {
+            AllUsers = await UserService.GetAllEmployees();
+        }
+    }
+}
